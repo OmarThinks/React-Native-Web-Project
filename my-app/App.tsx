70fe46ac2fdfc3c5b5e047f3d0c8c8c98469fbdc
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-12-08 14:20:23 +0200

    Made on mouse enter event

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -3,9 +3,18 @@ import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: "green" }]}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View
+        style={{ width: 30, height: 30, backgroundColor: "red" }}
+        onPointerEnter={() => {
+          console.log("I'm in");
+        }}
+        onPointerLeave={() => {
+          console.log("I am out");
+        }}
+      />
       <Button title="hey" />
     </View>
   );
