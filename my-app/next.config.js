6d83489f520c93c838a6b358a245808b1843048f
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-12-09 14:31:22 +0200

    next config: full

--- my-app/next.config.js
+++ my-app/next.config.js
@@ -1,5 +1,17 @@
-module.exports = {
+const { withExpo } = require("@expo/next-adapter");
+
+/** @type {import('next').NextConfig} */
+const nextConfig = withExpo({
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: [
+    "react-native",
+    "expo",
+    // Add more React Native/Expo packages here...
+  ],
   experimental: {
     forceSwcTransforms: true,
   },
-};
+});
+
+module.exports = nextConfig;
