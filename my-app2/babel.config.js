33dc745aec8a519a68652ac52235a0b884fb586c
OmarThinks <24939461+OmarThinks@users.noreply.github.com>
2023-12-11 23:37:01 +0200

    Handled Module reslover

--- my-app2/babel.config.js
+++ my-app2/babel.config.js
@@ -1,7 +1,17 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    plugins: ["expo-router/babel"],
+    plugins: [
+      "expo-router/babel",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "^react-native$": "react-native-web",
+          },
+        },
+      ],
+    ],
     presets: ["babel-preset-expo"],
   };
 };
